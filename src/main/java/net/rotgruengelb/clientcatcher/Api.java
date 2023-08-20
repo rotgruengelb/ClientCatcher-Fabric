7f873628f9fa795261d57acd61502e9b7bb3b211
rotgruengelb <rotgrungelblab@gmail.com>
2023-08-20 23:48:16 +0200

    2.0.0

--- src/main/java/net/rotgruengelb/clientcatcher/Api.java
+++ src/main/java/net/rotgruengelb/clientcatcher/Api.java
@@ -1,6 +1,8 @@
 package net.rotgruengelb.clientcatcher;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.block.BambooBlock;
+import net.minecraft.block.CactusBlock;
 
 public class Api {
     public static String getPlayerBrand(GameProfile gameProfile) {
